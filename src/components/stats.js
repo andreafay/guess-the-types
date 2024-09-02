19c7db59eafa12fe2a63e3c014ed82cafc6dc6a5
andreafay <andrea.fay@kireygroup.com>
2024-09-02 17:19:24 +0200

    more resize

--- src/components/stats.js
+++ src/components/stats.js
@@ -3,9 +3,9 @@ import React from "react";
 const Stats = () => {
         return (
         <div>
-            <p className="text-lg">Lives remaining: </p>
-            <p className="text-lg">Pokémon guessed: </p>
-            <p className="text-lg">Record: </p>
+            <p className="text-md">Lives remaining: </p>
+            <p className="text-md">Pokémon guessed: </p>
+            <p className="text-md">Record: </p>
         </div>
     );
 };
