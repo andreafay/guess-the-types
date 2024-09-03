b38707518ef8d13f3d27fcd8e08a41ab73904623
andreafay <andrea.fay@kireygroup.com>
2024-09-03 19:45:43 +0200

    added text no wrap

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ function App() {
         <div id="title" className="my-1">
           <p className="text-4xl font-semibold">Guess the types!</p>
         </div>
-        <div className="width-full mr-44">
+        <div className="width-full text-nowrap mr-44">
           <Stats lives={lives} guessed={guessed} record={record} />
         </div>
         <Card
