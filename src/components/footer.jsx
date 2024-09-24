e62062ab62b17ee75dab04592efc8065ecad5c21
andreafay <andrea.fay@kireygroup.com>
2024-09-24 12:33:25 +0200

    fix footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <p className="opacity-40 text-xs text-center font-semibold mt-10">
+        <p className="opacity-40 text-xs text-center font-semibold dark: text-neutral-100">
           &copy; {new Date().getFullYear()} Andrea Fay.
         </p>
     )
